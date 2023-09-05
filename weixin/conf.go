ecbc4e9c0ddb2f8beaa6e214fa3d59695fe274f4
Centny <centny@gmail.com>
2023-09-05 12:10:47 +0800

    Revert "change filed name."
    This reverts commit 4fc65001a82c16eb14baafd1e8b2e065a62f9ae8.

--- weixin/conf.go
+++ weixin/conf.go
@@ -20,7 +20,7 @@ type Conf struct {
 	MpAppid        string
 	ManualUsername string
 	ManualPassword string
-	PayClient      *http.Client
+	ApiClient      *http.Client
 }
 
 func (c *Conf) Md5Sign(data string) string {
